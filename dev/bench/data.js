d30e93a227e8a8018dd9dd0516644c203fd7e823
github-action-benchmark <github@users.noreply.github.com>
2025-01-20 08:52:18 +0000

    add Rust Benchmark (cargo) benchmark result for b5a598d01c53bf8abbe2240be9141e6f22880493

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1737359571073,
+  "lastUpdate": 1737363138274,
   "repoUrl": "https://github.com/contentstech-com/lazycsv",
   "entries": {
     "Rust Benchmark": [
@@ -103,6 +103,60 @@ window.BENCHMARK_DATA = {
             "name": "Parsers/csv",
             "value": 286064198,
             "range": "± 827690",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "me@xiniha.dev",
+            "name": "Iha Shin",
+            "username": "XiNiHa"
+          },
+          "committer": {
+            "email": "me@xiniha.dev",
+            "name": "Iha Shin",
+            "username": "XiNiHa"
+          },
+          "distinct": true,
+          "id": "b5a598d01c53bf8abbe2240be9141e6f22880493",
+          "message": "add links to benchmarks",
+          "timestamp": "2025-01-20T17:50:05+09:00",
+          "tree_id": "1bca17d4d36ae90ea7f4d04afa42e8d16ec9ba08",
+          "url": "https://github.com/contentstech-com/lazycsv/commit/b5a598d01c53bf8abbe2240be9141e6f22880493"
+        },
+        "date": 1737363137410,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "Parsers/lazy_csv",
+            "value": 246305243,
+            "range": "± 1909404",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Parsers/lazy_csv (into_rows)",
+            "value": 258347352,
+            "range": "± 1041261",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Parsers/lazy_csv (raw)",
+            "value": 161869071,
+            "range": "± 267874",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Parsers/lazy_csv (into_rows, raw)",
+            "value": 162332931,
+            "range": "± 1810328",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Parsers/csv",
+            "value": 286866385,
+            "range": "± 2926657",
             "unit": "ns/iter"
           }
         ]
